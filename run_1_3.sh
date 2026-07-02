78456d26bb6b2ca80f761c98bdf6d7a71b4f9190
Bary Levy <barylevi@gmail.com>
2026-07-02 11:07:40 +0300

    1.3

--- run_1_3.sh
+++ run_1_3.sh
@@ -8,7 +8,7 @@ nebius ai job create \
     --args '-c "git clone -b bary-testing https://github.com/Apex-IL/architects-ex-1.git && cd architects-ex-1 && torchrun --standalone --nproc_per_node=8 solution_1_3.py"' \
     --platform gpu-h100-sxm \
     --preset 8gpu-128vcpu-1600gb \
-    --timeout 30m \
+    --timeout 40m \
     --env HF_TOKEN=$HF_TOKEN \
     --env HF_REPO_ID=$HF_REPO_ID \
     --volume computefilesystem-e00hnnpfn5rr5aavma:/mnt/data
